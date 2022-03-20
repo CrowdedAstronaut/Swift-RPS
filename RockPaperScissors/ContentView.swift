8826cd7ef86a408ee2f4bd7793f3f1f73e9fb15b
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-20 14:49:56 -0500

    Built basic UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	let moves = ["✊" ,"👋", "✌️"]
+	
+	@State private var computerChoice = Int.random(in: 0..<3)
+	@State private var shouldWin = Bool.random()
+	
+	var body: some View {
+		VStack {
+			Spacer()
+			
+			Text("Computer has played…")
+				.font(.headline)
+			Text(moves[computerChoice])
+				.font(.system(size: 200))
+			
+			if shouldWin {
+				Text("Which one wins?")
+					.foregroundColor(.green)
+					.font(.title)
+			} else {
+				Text("Which one loses?")
+					.foregroundColor(.red)
+					.font(.title)
+			}
+			
+			HStack {
+				ForEach(0..<3) { number in
+					Button(moves[number]) {
+						// handle move tap
+					}
+					.font(.system(size: 80))
+				}
+			}
+			
+			Spacer()
+			
+			Text("Score: ???")
+				.font(.subheadline)
+			
+			Spacer()
+		}
+		
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
+
